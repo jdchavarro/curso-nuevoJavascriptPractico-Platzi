5300e0de86e6094827eff0f2221ef961fa2551c7
jdchavarro <jd-chavarro@hotmail.com>
2022-08-29 22:00:16 -0500

    manipulacion del DOM con listener

--- scriptManipulacion.js
+++ scriptManipulacion.js
@@ -4,6 +4,17 @@ const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
 
+// form.addEventListener('submit', sumarInputValues)
+
+// function sumarInputValues(event) {
+//   // console.log({event});
+//   event.preventDefault(); // evita la accion por defecto del evento
+//   const sumaInputs = input1.value + input2.value;
+//   pResult.innerText = "Resultado: " + sumaInputs;
+// }
+
+btn.addEventListener('click', btnOnClick);
+
 function btnOnClick() {
     const sumaInputs = Number(input1.value) + Number(input2.value);
     pResult.innerHTML = "Resultado: " + sumaInputs;
